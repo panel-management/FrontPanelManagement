0f454b3475d226783196e19c1751dbd45003f564
Ehsan-Fouladi <fooehan3@gmail.com>
2025-06-27 14:34:57 +0330

    config nuxt ui and tailwindcss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,29 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
-  devtools: { enabled: true }
+    compatibilityDate: '2025-05-15',
+    devtools: {enabled: true},
+    css: ['~/assets/css/main.css'],
+    modules: ['@nuxt/ui'],
+    vite: {
+        plugins: [
+            tailwindcss(),
+        ],
+    },
+    ui: {
+        fonts: false,
+        colorMode: false
+    },
+    app: {
+        head: {
+            charset: "utf-8",
+            viewport: "width=device-width, initial-scale=1",
+            meta: [{"http-equiv": "X-UA-Compatible", content: "IE=edge"}],
+            htmlAttrs: {
+                dir: "rtl",
+                lang: "fa",
+                translate: "no"
+            },
+        }
+    },
 })
