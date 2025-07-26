0b4a903011d4736eb9654ff89dacb26893077c25
Ehsan-Fouladi <fooehan3@gmail.com>
2025-07-26 08:59:15 +0330

    create base component uploadfile

---
+++ components/Base/formUploadFile.vue
@@ -0,0 +1,16 @@
+<template>
+  <UFormField :label="label" :name="name" :required="required">
+    <UFileUpload v-model="model" color="neutral" highlight label="تصویر خود را در اینجا رها کنید." :description="description" class="w-full min-h-48" :class="{ 'blur-[1px]': disable }"/>
+  </UFormField>
+</template>
+<script setup lang="ts">
+const model = defineModel<File | undefined>()
+
+const {label, name, required, disable = false, description = ''} = defineProps<{
+  label: string,
+  name: string,
+  required: boolean
+  disable?: boolean,
+  description?: string,
+}>()
+</script>
