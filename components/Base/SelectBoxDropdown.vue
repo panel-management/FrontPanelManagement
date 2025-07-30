841cad5c57d9eee9af5382158697de279bc2f521
Ehsan-Fouladi <fooehan3@gmail.com>
2025-07-30 18:36:25 +0330

    create new base component selectbox dropdown

---
+++ components/Base/SelectBoxDropdown.vue
@@ -0,0 +1,13 @@
+<template>
+  <USelect v-model="model" :items="items" :placeholder="placeholder" :disabled="disable" class="w-full"
+           :class="{ 'blur-[1px]': disable }"/>
+</template>
+<script setup lang="ts">
+const model = defineModel<string>()
+
+const {items, disable = false, placeholder = ''} = defineProps<{
+  items: any,
+  placeholder?: string,
+  disable?: boolean
+}>()
+</script>
