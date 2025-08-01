a2de42f46c1868ca28a34b7aa9c45168aa0ee975
Ehsan-Fouladi <fooehan3@gmail.com>
2025-08-01 06:53:51 +0330

    the header added container responsve

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,15 +1,17 @@
 <template>
   <UApp :locale="ar">
-    <div>
-      <header class="w-full shadow-xs shadow-black p-4 px-5">
-        <TheHeader/>
-      </header>
-      <main class="p-[10px] w-full h-full flex justify-center items-center">
-        <NuxtLoadingIndicator/>
-        <NuxtRouteAnnouncer/>
-        <NuxtPage/>
-      </main>
+    <div class="w-full h-full flex flex-col justify-center items-center">
+      <div class="container w-full h-full">
+        <header class="w-full shadow-xs shadow-black p-4 px-5">
+          <TheHeader/>
+        </header>
+      </div>
     </div>
+    <main class="p-[10px] w-full h-full flex justify-center items-center">
+      <NuxtLoadingIndicator/>
+      <NuxtRouteAnnouncer/>
+      <NuxtPage/>
+    </main>
   </UApp>
 </template>
 <script setup lang="ts">
