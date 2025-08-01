e74b866e8012b36e87f61bafa57cc1b6e4012559
Ehsan-Fouladi <fooehan3@gmail.com>
2025-08-01 13:42:09 +0330

    create new component otp form and change color form input

--- components/Base/formInput.vue
+++ components/Base/formInput.vue
@@ -1,6 +1,6 @@
 <template>
   <UFormField :label="label" :name="name" :required="required">
-    <UInput v-model="model" :type="type" :placeholder="placeholder" :disabled="disable" size="lg" class="w-full" :class="{ 'blur-[1px]': disable }"/>
+    <UInput v-model="model" :type="type" :placeholder="placeholder" :disabled="disable" color="neutral" size="lg" class="w-full" :class="{ 'blur-[1px]': disable }"/>
   </UFormField>
 </template>
 <script setup lang="ts">
---
+++ components/Base/formOtp.vue
@@ -0,0 +1,15 @@
+<template>
+  <UFormField :label="label" :name="name" :required="required">
+    <UPinInput v-model="model" size="xl" color="neutral" otp type="number" :length="6" :disabled="disable" :class="{ 'blur-[1px]': disable }"/>
+  </UFormField>
+</template>
+<script setup lang="ts">
+const model = defineModel<number[]>()
+
+const {name, required, label='', disable = false} = defineProps<{
+  label?: string,
+  name: string,
+  required: boolean
+  disable?: boolean
+}>()
+</script>
