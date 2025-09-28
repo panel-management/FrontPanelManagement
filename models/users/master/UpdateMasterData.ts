5e938bf6851265f1da2cc20c94946bde402b35b1
Ehsan-Fouladi <fooehan3@gmail.com>
2025-09-29 01:54:32 +0330

    added model master for update

---
+++ models/users/master/UpdateMasterData.ts
@@ -0,0 +1,11 @@
+export interface UpdateMasterData {
+  fullName: string;
+  nationalCode: string;
+  phoneNumber: string;
+  age: string;
+  birthDate: string;
+  history: string;
+  certificates: string;
+  imageFile?: File;
+  imageUrl?: string;
+}
