18940523df3aeb87eaa9fdbafcc40dfc997a24d9
Ehsan-Fouladi <fooehan3@gmail.com>
2025-10-01 22:04:17 +0330

    change and clean code

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,48 +1,46 @@
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineNuxtConfig({
-    compatibilityDate: '2025-05-15',
-    devtools: {enabled: process.env.APP_DEBUG?.toLocaleLowerCase() === 'true'},
-    modules: ['@nuxt/ui', '@pinia/nuxt'],
-    css: ['~/assets/css/main.css'],
-    vite: {
-        plugins: [
-            tailwindcss(),
-        ],
+  compatibilityDate: "2025-05-15",
+  devtools: { enabled: process.env.APP_DEBUG?.toLocaleLowerCase() === "true" },
+  modules: ["@nuxt/ui", "@pinia/nuxt"],
+  css: ["~/assets/css/main.css"],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  app: {
+    head: {
+      charset: "utf-8",
+      viewport: "width=device-width, initial-scale=1",
+      meta: [{ "http-equiv": "X-UA-Compatible", content: "IE=edge" }],
+      htmlAttrs: {
+        dir: "rtl",
+        lang: "fa",
+        translate: "no",
+      },
     },
-    app: {
-        head: {
-            charset: "utf-8",
-            viewport: "width=device-width, initial-scale=1",
-            meta: [{"http-equiv": "X-UA-Compatible", content: "IE=edge"}],
-            htmlAttrs: {
-                dir: "rtl",
-                lang: "fa",
-                translate: "no"
-            }
-        }
+  },
+  ui: {
+    fonts: false,
+    colorMode: false,
+    theme: {
+      colors: [
+        "primary",
+        "secondary",
+        "tertiary",
+        "info",
+        "success",
+        "warning",
+        "error",
+      ],
     },
-    ui: {
-        fonts: false,
-        colorMode: false,
-        theme: {
-            colors: [
-                'primary',
-                'secondary',
-                'tertiary',
-                'info',
-                'success',
-                'warning',
-                'error'
-            ]
-        }
+  },
+  runtimeConfig: {
+    public: {
+      APP_NAME: process.env.APP_NAME,
+      APP_ENV: process.env.APP_ENV,
+      APP_URL: process.env.APP_URL ?? "http://localhost:3000",
+      API_URL: process.env.API_URL,
     },
-    runtimeConfig: {
-        public: {
-            APP_NAME: process.env.APP_NAME,
-            APP_ENV: process.env.APP_ENV,
-            APP_URL: process.env.APP_URL ?? "http://localhost:3000",
-            API_URL: process.env.API_URL,
-        }
-    }
-})
+  },
+});
