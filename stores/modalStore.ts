10456139741f314c670101092f9084d17eff77fb
Ehsan-Fouladi <fooehan3@gmail.com>
2025-10-01 22:00:41 +0330

    edit function open modal and more ...

--- stores/modalStore.ts
+++ stores/modalStore.ts
@@ -1,25 +1,30 @@
-import {defineStore} from "pinia";
+import { defineStore } from "pinia";
 
 export const useModalStore = defineStore("modal", () => {
-    const modals = ref<Record<string, boolean>>({
-        paymentAdd: false,
-        paymentEdit: false,
-        rankAdd: false,
-        rankEdit: false,
-        studentAdd: false,
-        studentEdit: false,
-        coachesAdd: false,
-        coachesEdit: false,
-        equipmentAdd: false,
-        supportAdd: false,
-    })
+  const modals = ref<Record<string, any>>({
+    paymentAdd: false,
+    paymentEdit: false,
+    rankAdd: false,
+    rankEdit: false,
+    studentAdd: false,
+    studentEdit: false,
+    coachesAdd: false,
+    coachesEdit: false,
+    equipmentAdd: false,
+    masterEdit: null,
+    supportAdd: false,
+  });
 
-    function toggleModal(name: string) {
-        modals.value[name] = !modals.value[name]
+  function toggleModal(name: string, payload: any = null) {
+    if (typeof modals.value[name] === "boolean") {
+      modals.value[name] = !modals.value[name];
+    } else {
+      modals.value[name] = modals.value[name] ? null : payload;
     }
+  }
 
-    return {
-        modals,
-        toggleModal,
-    }
+  return {
+    modals,
+    toggleModal,
+  };
 });
