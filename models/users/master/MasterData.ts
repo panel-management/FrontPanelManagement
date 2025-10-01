41b358f268e96829090757aa26c99b89f737af2e
Ehsan-Fouladi <fooehan3@gmail.com>
2025-10-01 21:57:16 +0330

    update models master

--- models/users/master/MasterData.ts
+++ models/users/master/MasterData.ts
@@ -4,12 +4,15 @@ import type {PaymentStatus} from "~/models/PaymentStatus";
 
 export interface MasterData {
     user_id: number
+    type: number
     fullName: string
     nationalCode: string
     phoneNumber: string
     history: string
     image: any
     active: Active
+    certificates: string
+    masterPlanId: string
     sport: Sport
     students: any[]
     subscriptionPayments: any[]
--- models/users/master/UpdateMasterData.ts
+++ models/users/master/UpdateMasterData.ts
@@ -1,3 +1,5 @@
+import type { Sport } from "~/models/sportAndBelt/sport";
+
 export interface UpdateMasterData {
   fullName: string;
   nationalCode: string;
@@ -6,6 +8,8 @@ export interface UpdateMasterData {
   birthDate: string;
   history: string;
   certificates: string;
+  sport: Sport;
+  sportId: number;
   imageFile?: File;
   imageUrl?: string;
 }
