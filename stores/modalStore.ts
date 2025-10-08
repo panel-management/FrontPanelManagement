79490253d79fbdec970a48b1ef81eef1da0a8bba
Ehsan-Fouladi <fooehan3@gmail.com>
2025-10-09 00:34:59 +0330

    change code modal open and close

--- stores/modalStore.ts
+++ stores/modalStore.ts
@@ -3,7 +3,9 @@ import { defineStore } from "pinia";
 export const useModalStore = defineStore("modal", () => {
   const modals = ref<Record<string, any>>({
     paymentAdd: false,
+    paymentAdminAdd: false,
     paymentEdit: false,
+    paymentAdminEdit: false,
     rankAdd: false,
     rankEdit: false,
     studentAdd: false,
@@ -11,15 +13,25 @@ export const useModalStore = defineStore("modal", () => {
     coachesAdd: false,
     coachesEdit: false,
     equipmentAdd: false,
-    masterEdit: null,
+    masterEdit: false,
     supportAdd: false,
   });
 
+  // function toggleModal(name: string, payload: any = null) {
+  //   if (typeof modals.value[name] === "boolean") {
+  //     modals.value[name] = !modals.value[name];
+  //   } else {
+  //     modals.value[name] = modals.value[name] ? null : payload;
+  //   }
+  // }
+
   function toggleModal(name: string, payload: any = null) {
-    if (typeof modals.value[name] === "boolean") {
-      modals.value[name] = !modals.value[name];
+    if (payload && typeof payload === "object") {
+      modals.value[name] = payload;
+    } else if (modals.value[name] && typeof modals.value[name] === "object") {
+      modals.value[name] = false;
     } else {
-      modals.value[name] = modals.value[name] ? null : payload;
+      modals.value[name] = !modals.value[name];
     }
   }
 
