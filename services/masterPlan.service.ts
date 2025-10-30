eba4f495c0b646817e56a147a0ec166991b85299
Ehsan-Fouladi <fooehan3@gmail.com>
2025-10-31 01:41:46 +0330

    added service get plan master student and create plan student

--- services/masterPlan.service.ts
+++ services/masterPlan.service.ts
@@ -1,14 +1,26 @@
 import type { MasterPlanData } from "~/models/plan/masterPlan/MasterPlanData";
 import type { UpdateMasterPlanData } from "~/models/plan/masterPlan/UpdateMasterPlanData";
+import type { CreatePlanStudent } from "~/models/plan/studentPlan/CreatePlanStudent";
 
 export const getPlanMasterOrAdminService = () => {
   return FetchApi("/financials/master-plans");
+};
+
+export const getPlanMasterByStudentService = () => {
+  return FetchApi("/financials/plans");
 };
 
 export const createMasterPlanService = (master: MasterPlanData) => {
   return FetchApi("/financials/master-plans", {
     method: "POST",
     body: master,
+  });
+};
+
+export const createPlanMasterByStudentService = (data: CreatePlanStudent) => {
+  return FetchApi("/financials/plans", {
+    method: "POST",
+    body: data,
   });
 };
 
@@ -24,6 +36,12 @@ export const updateMasterPlanService = (
 
 export const deleteMasterPlanService = (id: number) => {
   return FetchApi(`/financials/master-plans/${id}`, {
+    method: "DELETE",
+  });
+};
+
+export const deletePlanMasterByStudentService = (id: number) => {
+  return FetchApi(`/financials/plans/${id}`, {
     method: "DELETE",
   });
 };
