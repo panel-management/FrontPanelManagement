7fd3fa998e048e2ce3670118cdcbbeff2277d534
Ehsan-Fouladi <fooehan3@gmail.com>
2025-11-11 14:28:55 +0330

    added model student and belt

---
+++ models/sportAndBelt/belt.ts
@@ -0,0 +1,6 @@
+export interface Belt {
+  id: number;
+  color: string;
+  achievedByUsers: any[];
+  currentlyHeldByUsers: any[];
+}
---
+++ models/users/student/UpdateStudent.ts
@@ -0,0 +1,12 @@
+export interface UpdateStudent {
+  fullName: string;
+  nationalCode: string;
+  age: number;
+  birthDate: string;
+  phoneNumber: string;
+  phoneNumberEmergency: string;
+  address: string;
+  underSupervisionDoctor: boolean;
+  diseaseRecords: boolean;
+  beltIds: number[];
+}
