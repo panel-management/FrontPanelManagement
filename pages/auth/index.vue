2744d157431415dfab844ea41baf5a94e0fd92ef
Ehsan-Fouladi <fooehan3@gmail.com>
2025-11-18 21:24:03 +0330

    change login user to url and fix:design page student

--- pages/auth/index.vue
+++ pages/auth/index.vue
@@ -1,22 +1,21 @@
 <template>
-  <UApp :locale="ar" :toaster="{position: 'top-right'}">
+  <UApp :locale="ar" :toaster="{ position: 'top-right' }">
     <div class="w-full h-dvh px-7 flex justify-center items-center">
       <UForm v-if="step === 1" :schema="step1Schema" :state="stateStep1" @submit.prevent="onSubmitStep1"
-             class="w-full md:w-2/3 lg:w-1/2 xl:w-1/3 h-full flex flex-col items-center justify-center gap-5">
+        class="w-full md:w-2/3 lg:w-1/2 xl:w-1/3 h-full flex flex-col items-center justify-center gap-5">
         <div class="flex flex-col items-center-safe gap-1">
           <span class="font-medium text-xl md:text-3xl text-black leading-relaxed">ورود به سیستم</span>
           <span class="text-black/80 font-normal text-sm">لطفاً اطلاعات خود را وارد کنید</span>
         </div>
         <div class="w-full flex flex-col gap-5">
           <BaseFormInput v-model="stateStep1.phoneNumber" label="شماره تلفن" name="phoneNumber" type="text"
-                         :required="false" class="w-full"/>
+            :required="false" class="w-full" />
         </div>
         <UButton :loading="isLoading" type="submit" color="neutral"
-                 class="btn btn-outline flex justify-center text-base"
-                 label="ورود"/>
+          class="btn btn-outline flex justify-center text-base" label="ورود" />
       </UForm>
       <UForm v-else-if="step === 2" :schema="step2Schema" :state="stateStep2" @submit.prevent="onSubmitStep2"
-             class="w-full md:w-2/3 lg:w-1/2 xl:w-1/3 h-full flex flex-col items-center justify-center gap-5">
+        class="w-full md:w-2/3 lg:w-1/2 xl:w-1/3 h-full flex flex-col items-center justify-center gap-5">
         <div class="flex flex-col items-center-safe gap-1">
           <span class="font-medium text-xl md:text-3xl text-black leading-relaxed">ورود به سیستم</span>
           <span class="text-black/80 font-normal text-sm">
@@ -25,88 +24,87 @@
         </div>
         <div class="w-full">
           <BaseFormOtp v-model="stateStep2.otpArray" name="otpArray" :disable="false" :required="false"
-                       class="w-full flex justify-center items-center"/>
+            class="w-full flex justify-center items-center" />
         </div>
         <UButton :loading="isLoading" type="submit" color="neutral"
-                 class="btn btn-outline flex justify-center text-base"
-                 label="ورود به حساب کاربری"/>
+          class="btn btn-outline flex justify-center text-base" label="ورود به حساب کاربری" />
       </UForm>
       <UForm v-else-if="step === 3" :schema="step3Schema" :state="stateStep3" @submit.prevent="onSubmitStep3"
-             class="w-full md:w-2/3 lg:w-1/2 xl:w-1/3 h-full flex flex-col items-center justify-center gap-5">
+        class="w-full md:w-2/3 lg:w-1/2 xl:w-1/3 h-full flex flex-col items-center justify-center gap-5">
         <div class="flex flex-col items-center-safe gap-1">
           <span class="font-medium text-xl md:text-3xl text-black leading-relaxed">ثبت نام</span>
           <span class="text-black/80 font-normal text-sm">ایجاد حساب کاربری جدید</span>
         </div>
         <div class="w-full flex flex-col gap-5">
           <BaseFormInput v-model="stateStep3.fullName" label="نام و نام خانوادگی" name="fullName" type="text" required
-                         :disable="false" class="w-full"/>
+            :disable="false" class="w-full" />
           <BaseFormInput v-model="stateStep3.nationalCode" label="کد ملی" name="nationalCode" type="text" required
-                         :disable="false" class="w-full"/>
+            :disable="false" class="w-full" />
           <BaseFormInput v-model="stateStep1.phoneNumber" label="شماره تلفن" name="phoneNumber" type="text" required
-                         :disable="true" class="w-full"/>
+            :disable="true" class="w-full" />
           <BaseFormInput v-model="otpValue" label="کد تایید" name="otpArray" type="text" required :disable="true"
-                         class="w-full"/>
+            class="w-full" />
           <BaseFormSelect required :disable="false" v-model="stateStep3.selectSport" :items="itemsSelect"
-                          name="selectSport" placeholder="رشته مورد نظر را انتخاب کنید" label="انتخاب رشته ورزشی"/>
+            name="selectSport" placeholder="رشته مورد نظر را انتخاب کنید" label="انتخاب رشته ورزشی" />
         </div>
         <UButton :loading="isLoading" type="submit" color="neutral"
-                 class="btn btn-outline flex justify-center text-base"
-                 label="ثبت نام"/>
+          class="btn btn-outline flex justify-center text-base" label="ثبت نام" />
       </UForm>
     </div>
   </UApp>
 </template>
 <script setup lang="ts">
-import {ar} from "#ui/locale";
+import { ar } from "#ui/locale";
 import * as v from "valibot";
-import type {FormSubmitEvent} from "@nuxt/ui";
-import {otpCodeService, registerUsersService, requestOtpService} from "~/services/auth.service";
-import {getAllSportService} from "~/services/sportBelt.service";
-import type {Sport} from "~/models/sportAndBelt/sport";
+import type { FormSubmitEvent } from "@nuxt/ui";
+import { otpCodeService, registerUsersService, requestOtpService } from "~/services/auth.service";
+import { getAllSportService } from "~/services/sportBelt.service";
+import type { Sport } from "~/models/sportAndBelt/sport";
 
 const step: Ref<number> = ref(1)
 const isLoading: Ref<boolean> = ref(false)
 const toastStore = useToastStore()
 const accountStore = useAccountStore()
 const router = useRouter()
+const route = useRoute()
 const itemsSelect: Ref<any[]> = ref([])
 const formData: Ref<Sport[]> = ref([])
 
 const step1Schema = v.object({
   phoneNumber: v.pipe(
-      v.string(),
-      v.trim(),
-      v.nonEmpty('شماره تلفن الزامی است'),
-      v.minLength(11, 'شماره تلفن باید حداقل ۱۱ رقم باشد'),
-      v.maxLength(12, 'شماره تلفن نباید بیشتر از ۱۲ رقم باشد'),
-      v.custom((value) => /^\d+$/.test(value), 'شماره تلفن فقط می‌تواند شامل اعداد باشد')
+    v.string(),
+    v.trim(),
+    v.nonEmpty('شماره تلفن الزامی است'),
+    v.minLength(11, 'شماره تلفن باید حداقل ۱۱ رقم باشد'),
+    v.maxLength(12, 'شماره تلفن نباید بیشتر از ۱۲ رقم باشد'),
+    v.custom((value) => /^\d+$/.test(value), 'شماره تلفن فقط می‌تواند شامل اعداد باشد')
   ),
 })
 const step2Schema = v.object({
   otpArray: v.pipe(
-      v.array(v.number()),
-      v.nonEmpty('کد تایید الزامی است'),
-      v.minLength(6, 'کد باید ۶ رقم باشد'),
-      v.maxLength(6, 'کد نباید بیشتر از ۶ رقم باشد')
+    v.array(v.number()),
+    v.nonEmpty('کد تایید الزامی است'),
+    v.minLength(6, 'کد باید ۶ رقم باشد'),
+    v.maxLength(6, 'کد نباید بیشتر از ۶ رقم باشد')
   ),
 })
 const step3Schema = v.object({
   fullName: v.pipe(
-      v.string(),
-      v.trim(),
-      v.nonEmpty('نام و نام خانوادگی مربی الزامی است.')
+    v.string(),
+    v.trim(),
+    v.nonEmpty('نام و نام خانوادگی مربی الزامی است.')
   ),
   nationalCode: v.pipe(
-      v.string(),
-      v.trim(),
-      v.nonEmpty('کد ملی الزامی است.'),
-      v.maxLength(10, 'کد ملی دارای 10 رقم میباشد لطف مجدد وارد کنید.'),
-      v.custom((value) => /^\d+$/.test(value), 'کد ملی فقط می‌تواند شامل اعداد باشد')
+    v.string(),
+    v.trim(),
+    v.nonEmpty('کد ملی الزامی است.'),
+    v.maxLength(10, 'کد ملی دارای 10 رقم میباشد لطف مجدد وارد کنید.'),
+    v.custom((value) => /^\d+$/.test(value), 'کد ملی فقط می‌تواند شامل اعداد باشد')
   ),
   selectSport: v.pipe(
-      v.string(),
-      v.trim(),
-      v.minLength(1, 'لطفا یک از موارد رشته انتخاب کنید.'),
+    v.string(),
+    v.trim(),
+    v.minLength(1, 'لطفا یک از موارد رشته انتخاب کنید.'),
   )
 })
 
@@ -160,7 +158,7 @@ async function onSubmitStep2(event: FormSubmitEvent<step2Schema>) {
       toastStore.setAlert(result.message, '', 'success', 'ep:success-filled')
       accountStore.setAuthToken(result.data)
       setTimeout(() => {
-        router.push('/dashboard')
+        navigateTo(route.query.redirectTo as string, { replace: true })
       }, 200)
     } else if (result.statusCode === 404) {
       step.value = 3
--- pages/management-students/index.vue
+++ pages/management-students/index.vue
@@ -6,21 +6,45 @@
         <span class="text-xs sm:text-sm font-medium">مشاهده و مدیریت اطلاعات هنرجویان باشگاه</span>
       </div>
       <div class="sm:p-2">
-        <UButton @click="modalStore.toggleModal('studentAdd')" trailing-icon="material-symbols:person-add" size="lg" color="primary" variant="subtle">
+        <UButton @click="modalStore.toggleModal('studentAdd')" trailing-icon="material-symbols:person-add" size="lg"
+          color="primary" variant="subtle">
           افزودن هنرجو جدید
         </UButton>
       </div>
     </div>
     <div class="bg-white flex flex-col gap-3 p-3 rounded-lg">
       <div class="flex flex-col gap-2">
-        <span class="text-2xl font-bold">لیست هنرجویان (5 نفر)</span>
-        <p class="break-words font-medium text-sm">مشاهده کامل اطلاعات هنرجویان و مدیریت آنها</p>
+        <span class="text-2xl font-bold">لیست هنرجویان ({{ formData.length }} نفر)</span>
+        <p class="wrap-break-word font-medium text-sm">مشاهده کامل اطلاعات هنرجویان و مدیریت آنها</p>
       </div>
-      <LazyTableStudentsTable/>
+      <TableStudentsTable :items="formData" :loading="isLoading" @refresh="getAllStudents" />
     </div>
-    <LazyWidgetModalStudentAdd v-model:open="modalStore.modals.studentAdd"/>
+    <LazyWidgetModalStudentAdd v-model:open="modalStore.modals.studentAdd" @success="getAllStudents" />
+    <LazyWidgetModalStudentEdit v-model:open="modalStore.modals.studentEdit" @success="getAllStudents" />
   </section>
 </template>
 <script setup lang="ts">
+import type { StudentData } from '~/models/users/student/StudentData';
+import { getAllStudentService } from '~/services/student.service';
+
 const modalStore = useModalStore()
+const formData = shallowRef<StudentData[]>([])
+const isLoading: Ref<boolean> = ref(false)
+
+async function getAllStudents() {
+  isLoading.value = true
+  try {
+    const result = await getAllStudentService()
+    console.log(result.data);
+    if (result.statusCode === 200) {
+      formData.value = Array.isArray(result.data) ? result.data : []
+    }
+  } catch (error: any) {
+    console.log(error.message || error);
+  } finally {
+    isLoading.value = false
+  }
+}
+
+onMounted(getAllStudents)
 </script>
