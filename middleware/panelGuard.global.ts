75d24330f379395b51dbd464f4e9b32d23e49bc5
Ehsan-Fouladi <fooehan3@gmail.com>
2025-11-18 21:24:29 +0330

    change route login user

--- middleware/panelGuard.global.ts
+++ middleware/panelGuard.global.ts
@@ -1,16 +1,16 @@
 export default defineNuxtRouteMiddleware((to, from) => {
-    const accountStore = useAccountStore();
-    const publicPage = ['/']
-    const isAuthPage = to.path.toLocaleLowerCase().startsWith("/auth")
-    const isPublicPage = publicPage.includes(to.path)
+  const accountStore = useAccountStore();
+  const publicPage = ["/"];
+  const isAuthPage = to.path.toLocaleLowerCase().startsWith("/auth");
+  const isPublicPage = publicPage.includes(to.path);
 
-    if (!isAuthPage && !isPublicPage) {
-        if (!accountStore.isLogin) {
-            return navigateTo(`/auth?redirect=${encodeURIComponent(to.fullPath)}`, {replace: true})
-        }
-    } else {
-        if (accountStore.isLogin && (isAuthPage || isPublicPage)) {
-            return navigateTo("/dashboard", {replace: true})
-        }
+  if (!isAuthPage && !isPublicPage) {
+    if (!accountStore.isLogin) {
+      return navigateTo(`/auth?redirectTo=${to.path}`);
     }
-})
+  } else {
+    if (accountStore.isLogin && (isAuthPage || isPublicPage)) {
+      return navigateTo("/dashboard", { replace: true });
+    }
+  }
+});
