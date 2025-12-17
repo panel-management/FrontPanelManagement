b227cbfcf7a695339f8790cace9f6f8b549e0fc8
Ehsan-Fouladi <fooehan3@gmail.com>
2025-12-17 23:04:45 +0330

    create service ticket

---
+++ services/ticket.service.ts
@@ -0,0 +1,36 @@
+import type { CreateTicket } from "~/models/ticket/CreateTicket";
+import type { SendMassage } from "~/models/ticket/SendMassage";
+import { TicketStatus } from "~/models/ticket/TicketData";
+
+export const getTicketMasterService = (page: number, limit: number) => {
+  return FetchApi(`/tickets?page=${page}&limit=${limit}`);
+};
+
+export const getTicketAdminService = (page: number, limit: number) => {
+  return FetchApi(`/tickets/admin/all?page=${page}&limit=${limit}`);
+};
+
+export const getTicketByIdService = (id: number) => {
+  return FetchApi(`/tickets/${id}`);
+};
+
+export const changeStatusTicketService = (id: number) => {
+  return FetchApi(`/tickets/${id}/status`, {
+    method: "PUT",
+    body: { status: TicketStatus.CLOSED },
+  });
+};
+
+export const createTicketService = (data: CreateTicket) => {
+  return FetchApi("/tickets", {
+    method: "POST",
+    body: data,
+  });
+};
+
+export const sendMessageService = (id: number, data: SendMassage) => {
+  return FetchApi(`/tickets/${id}/message`, {
+    method: "POST",
+    body: data,
+  });
+};
