463424c774dc7f9c04efc619bf80b3ecfff46bb5
Ehsan-Fouladi <fooehan3@gmail.com>
2025-12-17 23:06:26 +0330

    create badge ticket and create support add

--- components/Widget/Modal/Support/Add.vue
+++ components/Widget/Modal/Support/Add.vue
@@ -1,8 +1,12 @@
 <script setup lang="ts">
 import * as v from 'valibot'
-import type {FormSubmitEvent} from "@nuxt/ui"
+import type { FormSubmitEvent } from "@nuxt/ui"
+import type { CreateTicket } from '~/models/ticket/CreateTicket';
+import { createTicketService } from '~/services/ticket.service';
 
-const emit = defineEmits(['update:open']);
+const emit = defineEmits(['update:open', 'refresh']);
+const toastStore = useToastStore()
+const isLoading: Ref<boolean> = ref(false)
 
 const props = defineProps({
   open: {
@@ -16,71 +20,96 @@ const localOpen = computed({
   set: (Val) => emit('update:open', Val)
 });
 
-const categorySelect = ref(['مشکل فنی', 'امور مالی', 'در خواست ویژگی', 'عمومی'])
-const prioritySelect = ref(['کم', 'متوسط', 'زیاد'])
+// const categorySelect = ref(['مشکل فنی', 'امور مالی', 'در خواست ویژگی', 'عمومی'])
+// const prioritySelect = ref(['کم', 'متوسط', 'زیاد'])
+// const categorySelect: Ref<TicketCategory[]> = ref(
+//   [TicketCategory.FEATURE_REQUEST, TicketCategory.FINANCIAL_AFFAIRS, TicketCategory.GENERAL, TicketCategory.TECHNICAL_ISSUE]
+// )
+// const prioritySelect: Ref<TicketPriority[]> = ref([TicketPriority.LOW, TicketPriority.MEDIUM, TicketPriority.HIGH])
 
 const schema = v.object({
-  ticket: v.pipe(
-      v.string(),
-      v.trim(),
-      v.nonEmpty('عنوان تیکت الزامی است.')
+  title: v.pipe(
+    v.string(),
+    v.trim(),
+    v.nonEmpty('عنوان تیکت الزامی است')
   ),
-  description: v.pipe(
-      v.string(),
-      v.trim(),
-      v.nonEmpty('توضیحات الزامی است.')
+  text: v.pipe(
+    v.string(),
+    v.trim(),
+    v.nonEmpty('توضیحات الزامی است')
   ),
   category: v.pipe(
-      v.string(),
-      v.trim(),
-      v.minLength(1, 'لطفا یک از موارد دسته ‌بندی انتخاب کنید.'),
-      v.custom((val: any) => categorySelect.value.includes(val), 'دسته‌بندی انتخاب ‌شده معتبر نیست.')
+    v.string(),
+    v.trim(),
+    v.nonEmpty('دسته بندی الزامی است'),
+    v.minLength(1, 'لطفا یک از موارد دسته ‌بندی انتخاب کنید'),
   ),
   priority: v.pipe(
-      v.string(),
-      v.trim(),
-      v.minLength(1, 'لطفا یک از موارد اولویت انتخاب کنید.'),
-      v.custom((val: any) => prioritySelect.value.includes(val), 'اولویت انتخاب ‌شده معتبر نیست.')
+    v.string(),
+    v.trim(),
+    v.nonEmpty('موارد الویت الزامی است'),
+    v.minLength(1, 'لطفا یک از موارد اولویت انتخاب کنید'),
   )
 })
 
 type Schema = v.InferOutput<typeof schema>;
 
-const state = reactive({
-  ticket: '',
-  description: '',
+const state = reactive<CreateTicket>({
+  title: '',
+  text: '',
   category: '',
   priority: '',
 });
 
 async function onSubmit(event: FormSubmitEvent<Schema>) {
-  console.log(event.data)
+  isLoading.value = true
+  try {
+    const result = await createTicketService(event.data as CreateTicket);
+    console.log(result)
+    if (result.statusCode === 201) {
+      toastStore.setAlert(result.message, '', 'success', 'ep:success-filled')
+      localOpen.value = false
+      resetForm()
+      emit('refresh')
+    }
+  } catch (error: any) {
+    console.log(error.message || error);
+  } finally {
+    isLoading.value = false
+  }
+}
+
+async function resetForm() {
+  state.title = '';
+  state.text = '';
+  state.category = '';
+  state.priority = '';
 }
 </script>
 
 <template>
-  <UModal v-model:open="localOpen" title=" ثبت تیکت جدید" description="مشکل یا درخواست خود را ثبت کنید"
-          :ui="{ footer: 'justify-between' }">
+  <UModal v-model:open="localOpen" title="ثبت تیکت جدید" description="مشکل یا درخواست خود را ثبت کنید"
+    :ui="{ footer: 'justify-between' }">
     <template #body>
       <UForm :schema="schema" :state="state" @submit.prevent="onSubmit">
         <div class="flex flex-col gap-5 w-full">
           <div class="w-full">
-            <BaseFormInput v-model="state.ticket" label="عنوان تیکت" name="ticket" type="text"
-                           placeholder="عنوان مشکل یا درخواست خود را بنویسید." required class="w-full"/>
+            <BaseFormInput v-model="state.title" label="عنوان تیکت" name="title" type="text"
+              placeholder="عنوان مشکل یا درخواست خود را بنویسید." required class="w-full" />
           </div>
           <div class="flex max-sm:flex-col gap-5 sm:gap-3 w-full">
             <BaseFormSelect :required="true" v-model="state.category" :items="categorySelect" name="category"
-                            placeholder="دسته بندی را انتخاب کنید" label="دسته بندی" class="w-full"/>
+              placeholder="دسته بندی را انتخاب کنید" label="دسته بندی" class="w-full" />
             <BaseFormSelect :required="true" v-model="state.priority" :items="prioritySelect" name="priority"
-                            placeholder="اولویت را انتخاب کنید" label="اولویت" class="w-full"/>
+              placeholder="اولویت را انتخاب کنید" label="اولویت" class="w-full" />
           </div>
           <div class="w-full">
-            <BaseFormTextArea v-model="state.description" label="توضیحات" name="description"
-                              placeholder="مشکل یا درخواست خود را شرح دهید." required class="w-full"/>
+            <BaseFormTextArea v-model="state.text" label="توضیحات" name="text"
+              placeholder="مشکل یا درخواست خود را شرح دهید." required class="w-full" />
           </div>
           <div class="flex justify-between gap-2 pt-4">
-            <UButton label="انصراف" color="neutral" variant="outline" @click="localOpen = false"/>
-            <UButton label="ارسال تیکت" color="primary" type="submit"/>
+            <UButton label="انصراف" color="neutral" variant="outline" @click="localOpen = false" />
+            <UButton label="ارسال تیکت" color="primary" type="submit" />
           </div>
         </div>
       </UForm>
