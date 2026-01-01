1fb9e68861bd2307289960586c22c9b296bc2e69
Ehsan-Fouladi <fooehan3@gmail.com>
2026-01-01 22:09:04 +0330

    change design plan cart master layout

--- components/Carts/Plans.vue
+++ components/Carts/Plans.vue
@@ -1,18 +1,16 @@
 <template>
-  <section class="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 lg:pt-6 max-lg:px-2 lg:px-2">
+  <div class="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 place-items-center-safe gap-2 lg:pt-6 max-lg:px-2 lg:px-2">
     <div v-for="data in activePlanDataMaster" :key="data.id"
       class="relative w-full md:max-w-sm bg-white rounded-2xl p-8 text-center border-2 border-sky-200 flex flex-col gap-10 transition-all duration-300 md:hover:-translate-y-2 hover:shadow-xl">
       <div class="flex flex-col gap-3">
         <h2 class="text-2xl font-bold text-black">{{ data.name }}</h2>
         <p class="text-base text-gray-500">{{ data.description }}</p>
       </div>
-
       <div class="flex flex-col gap-2">
         <span class="text-5xl font-extrabold text-black" v-if="Number(data.price) === 0">رایگان</span>
         <span class="text-5xl font-extrabold text-black" v-else>{{ Number(data.price).toLocaleString('fa-IR') }}</span>
         <span class="block text-sm text-gray-500">به مدت {{ data.durationInDays }} روز</span>
       </div>
-
       <ul class="list-none flex flex-col flex-grow gap-3">
         <li class="flex items-center gap-2" v-for="feature in data.features">
           <UIcon name="clarity:success-line" class="size-5 text-teal-500" />
@@ -26,16 +24,14 @@
           <span class="text-base text-black">استفاده از کمدهای عمومی</span>
         </li> -->
       </ul>
-
-      <!-- <button class="w-full py-4 px-5 bg-[#457b9d] text-white rounded-xl text-lg font-bold cursor-pointer transition-all duration-300 hover:bg-[#1d3557] hover:-translate-y-1"></button> -->
       <UButton :loading="isLoading[data.id]" @click="selectPlanMaster(data.id)"
         class="py-3 font-medium text-lg flex place-content-center" color="tertiary" variant="solid"
-        :label="data.type === 'TRIAL' ? 'شروع دوره آزمایشی' : 'انتخاب'" />
+        :label="data.type === TypePlan.TRIAL ? 'شروع دوره آزمایشی' : 'انتخاب'" />
     </div>
-  </section>
+  </div>
 </template>
 <script setup lang="ts">
-import type { MasterPlanData } from '~/models/plan/masterPlan/MasterPlanData';
+import { TypePlan, type MasterPlanData } from '~/models/plan/masterPlan/MasterPlanData';
 import { selectPlanYourSelfMasterService } from '~/services/master.service';
 import { getPlanMasterOrAdminService } from '~/services/masterPlan.service';
 
