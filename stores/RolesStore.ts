73bf00a2ba35b080e7994bca428c191c2e0431ce
Ehsan-Fouladi <fooehan3@gmail.com>
2026-02-17 03:38:36 +0330

    fix: time show toast and added request error 403 ban account to role user

--- stores/RolesStore.ts
+++ stores/RolesStore.ts
@@ -4,17 +4,21 @@ import { getDataUserService } from '~/services/users.service'
 
 export const useRolesStore = defineStore('role', () => {
   const detailUser = ref<DataUsers | null>(null)
+  const isBanned: Ref<boolean> = ref(false)
 
   async function getDetailUser() {
     try {
       const result = await getDataUserService()
       if (result.statusCode === 200) {
         detailUser.value = result.data as DataUsers
+        isBanned.value = false
+      } else if (result.statusCode === 403) {
+        isBanned.value = true
       }
     } catch (error: any) {
       console.log(error.message || error)
     }
   }
 
-  return { detailUser, getDetailUser }
+  return { detailUser, getDetailUser, isBanned }
 })
--- stores/toastStore.ts
+++ stores/toastStore.ts
@@ -1,13 +1,13 @@
 export const useToastStore = defineStore('toast', () => {
   const toast = useToast()
 
-  const setAlert = (title?: string, description?: string, color: string, icon?: string) => {
+  const setAlert = (title: string, description?: string, color: string, icon: string) => {
     toast.add({
       title: title,
       description: description,
       color: color,
       icon: icon,
-      duration: 2000,
+      duration: 3000,
     })
   }
 
